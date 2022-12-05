0e8cf1672deb7945a42594fe66b3cbc5e1e92ae5
spacemandev <devbharel@gmail.com>
2022-12-04 23:10:36 -0600

    playing around with xnft, wasm loader not working

--- sdk/dominari/src/lib.rs
+++ sdk/dominari/src/lib.rs
@@ -6,3 +6,10 @@ pub mod gamestate;
 
 // Export Solana Client so no need to reimport it
 pub use solana_client_wasm::solana_sdk;
+
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn greet() -> String {
+    "Hello World!".to_string()
+}
